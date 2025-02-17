c5d0d9411b98450fc8c5ae4ab40a67cd4a971659
João Antônio <joao@debian>
2025-02-17 19:37:25 -0300

    Update

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'vendas',
+  webDir: 'dist'
+};
+
+export default config;
