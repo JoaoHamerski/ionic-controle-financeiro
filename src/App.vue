a89601ac55f4ab951519f14c09b73a0270fc27f5
João Antônio Hamerski Copetti <joaohamerski@outlook.com>
2025-03-02 05:25:48 -0300

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
+import { seedDatabase } from '@/database/seed'
 import { IonApp, IonRouterOutlet } from '@ionic/vue'
 import { onMounted } from 'vue'
 import { useDatabaseStore } from './database/database-store'
-
 const databaseStore = useDatabaseStore()
 
-onMounted(() => {
-  databaseStore.initDatabase()
+onMounted(async () => {
+  await databaseStore.initDatabase()
+  await seedDatabase()
 })
 </script>
 
---
+++ src/database/seed.ts
@@ -0,0 +1,37 @@
+import { dbInsert } from '@/services/db-service'
+import { SQLiteDBConnection } from '@capacitor-community/sqlite'
+import { fakerPT_BR as faker } from '@faker-js/faker'
+import { Knex } from 'knex'
+import { useDatabaseStore } from './database-store'
+import { Customer } from './models'
+
+export const seedDatabase = async () => {
+  const { database, builder } = useDatabaseStore()
+
+  await truncateAllTables(database)
+  await seedCustomers(database, builder)
+}
+
+const truncateAllTables = async (database: SQLiteDBConnection) => {
+  const tables = await database.query("SELECT name FROM sqlite_master WHERE type='table'")
+
+  for (const table of tables.values || []) {
+    await database.execute(`DELETE FROM ${table.name}`)
+  }
+}
+
+const seedCustomers = async (database: SQLiteDBConnection, builder: Knex) => {
+  const customers: Customer[] = []
+  const quantity = faker.number.int({ min: 20, max: 40 })
+
+  for (let i = 0; i < quantity; i++) {
+    customers.push({
+      id: i,
+      name: faker.person.fullName(),
+      phone: faker.string.numeric({ length: { min: 10, max: 11 } }),
+      created_at: faker.date.past().toISOString(),
+    })
+  }
+
+  await dbInsert(builder.insert(customers).into('customers'))
+}
