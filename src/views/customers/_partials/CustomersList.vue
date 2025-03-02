45de19ddf2540a6088a053034f361531a92c8121
João Antônio Hamerski Copetti <joaohamerski@outlook.com>
2025-03-02 05:50:40 -0300

    Update

--- src/views/customers/_partials/CustomersList.vue
+++ src/views/customers/_partials/CustomersList.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import { Customer } from '@/database/models'
-import { IonItem, IonLabel, IonList } from '@ionic/vue'
-
+import { AppLauncher } from '@capacitor/app-launcher'
+import { IonButton, IonIcon, IonItem, IonLabel, IonList } from '@ionic/vue'
+import { logoWhatsapp } from 'ionicons/icons'
 defineProps<{
   customers: Customer[]
 }>()
+
+const openInWhatsApp = async () => {
+  await AppLauncher.canOpenUrl({ url: 'whatsapp.com' })
+}
 </script>
 
 <template>
@@ -13,7 +18,21 @@ defineProps<{
       v-for="customer in customers"
       :key="customer.id"
     >
-      <IonLabel>{{ customer.name }}</IonLabel>
+      <IonLabel>
+        {{ customer.name }}
+      </IonLabel>
+      <IonButton
+        fill="clear"
+        color="success"
+        size="lg"
+        shape="round"
+        @click="openInWhatsApp"
+      >
+        <IonIcon
+          slot="icon-only"
+          :icon="logoWhatsapp"
+        />
+      </IonButton>
     </IonItem>
   </IonList>
 </template>
