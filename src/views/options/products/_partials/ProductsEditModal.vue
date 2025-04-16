14e54d0d43c3ee790a4ccd460fa8827533406869
João Antônio Hamerski Copetti <joaohamerski@outlook.com>
2025-04-16 13:42:32 -0300

    Update

--- src/views/options/products/_partials/ProductsEditModal.vue
+++ src/views/options/products/_partials/ProductsEditModal.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-import { IonButton, IonModal } from '@ionic/vue'
+import { IonModal } from '@ionic/vue'
+import { PhPackage } from '@phosphor-icons/vue'
 import { useTemplateRef } from 'vue'
 
+import AppModalHeader from '@/components/AppModalHeader.vue'
 import { presentToast } from '@/support/toast'
 
 import { ProductRecord } from '../ProductsPage.vue'
@@ -15,12 +17,9 @@ defineProps<{
 
 const modal = useTemplateRef('modal')
 
-const closeModal = () => {
-  modal.value?.$el.dismiss()
-}
-
 const onFormSubmit = () => {
-  closeModal()
+  modal.value?.$el.dismiss()
+
   emit('submitted')
   presentToast({ message: 'Produto atualizado', color: 'success' })
 }
@@ -31,33 +30,19 @@ const onFormSubmit = () => {
     ref="modal"
     class="modal-dialog"
   >
-    <div class="ion-margin">
-      <h6 style="margin-top: 0">Altere o nome do produto</h6>
+    <div class="ion-padding">
+      <AppModalHeader
+        title="Altere o nome do produto"
+        :icon="PhPackage"
+        class="text-[var(--ion-color-primary-shade)]"
+      />
 
-      <div class="ion-margin-bottom ion-margin-top">
-        <ProductForm
-          id="product-form"
-          :product="product"
-          @submit="onFormSubmit"
-        />
-      </div>
-
-      <div :style="{ display: 'flex', justifyContent: 'space-between' }">
-        <IonButton
-          fill="clear"
-          @click="closeModal"
-        >
-          Cancelar
-        </IonButton>
-
-        <IonButton
-          type="submit"
-          fill="solid"
-          form="product-form"
-        >
-          Alterar
-        </IonButton>
-      </div>
+      <ProductForm
+        id="product-form"
+        :product="product"
+        @submitted="onFormSubmit"
+        @cancel="modal?.$el.dismiss()"
+      />
     </div>
   </IonModal>
 </template>
--- src/views/options/products/_partials/ProductsForm.vue
+++ src/views/options/products/_partials/ProductsForm.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
+import { IonButton, IonCol, IonGrid, IonRow } from '@ionic/vue'
 import { helpers, required } from '@vuelidate/validators'
 import { onMounted, useTemplateRef } from 'vue'
+import { ref } from 'vue'
 
 import AppInput from '@/components/AppInput.vue'
 import { useForm } from '@/composables/use-form'
@@ -11,11 +13,13 @@ import { ProductRecord } from '../ProductsPage.vue'
 
 const { knex } = useDatabaseStore()
 
-const emit = defineEmits(['submitted'])
+const emit = defineEmits(['submitted', 'cancel'])
 
 const props = defineProps<{
   product?: ProductRecord
 }>()
+
+const isLoading = ref(false)
 
 const form = useForm(
   {
@@ -41,6 +45,8 @@ const submit = async () => {
     return
   }
 
+  isLoading.value = true
+
   const builder = knex
     .table('products')
     .update({ name: form.data.name })
@@ -57,13 +63,41 @@ const submit = async () => {
     @submit.prevent="submit"
     @focus.capture="form.clearErrorOnFocus"
   >
-    <AppInput
-      ref="input"
-      v-model="form.data.name"
-      label="Nome"
-      name="name"
-      :error="form.errors.name"
-      autofocus
-    />
+    <IonGrid>
+      <IonRow class="ion-margin-bottom">
+        <IonCol>
+          <AppInput
+            ref="input"
+            v-model="form.data.name"
+            label="Nome"
+            name="name"
+            :error="form.errors.name"
+            autofocus
+          />
+        </IonCol>
+      </IonRow>
+      <IonRow>
+        <IonCol>
+          <IonButton
+            fill="clear"
+            class="w-full"
+            shape="round"
+            @click="$emit('cancel')"
+          >
+            Cancelar
+          </IonButton>
+        </IonCol>
+        <IonCol>
+          <IonButton
+            shape="round"
+            class="w-full"
+            type="submit"
+            :disabled="isLoading"
+          >
+            Salvar
+          </IonButton>
+        </IonCol>
+      </IonRow>
+    </IonGrid>
   </form>
 </template>
