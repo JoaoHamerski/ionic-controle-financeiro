1280f6193d80c1d08e5161d9bf834eb341fe0dcc
João Antônio Hamerski Copetti <joaohamerski@outlook.com>
2025-04-16 14:08:44 -0300

    Update

--- src/views/home/_partials/EntryPaymentModal.vue
+++ src/views/home/_partials/EntryPaymentModal.vue
@@ -1,18 +1,13 @@
 <script setup lang="ts">
-import { IonButton, IonCol, IonGrid, IonModal, IonRow } from '@ionic/vue'
-import { maskito as vMaskito } from '@maskito/vue'
+import { IonModal } from '@ionic/vue'
 import { PhCurrencyCircleDollar } from '@phosphor-icons/vue'
 import { useTemplateRef } from 'vue'
 
-import AppInput from '@/components/AppInput.vue'
 import AppModalHeader from '@/components/AppModalHeader.vue'
-import { useForm } from '@/composables/use-form'
-import { currencyBrlMask } from '@/support/masks'
+
+import EntryPaymentModalForm from './EntryPaymentModalForm.vue'
 
 const modal = useTemplateRef('modal')
-const form = useForm({
-  value: 'R$ ',
-})
 </script>
 
 <template>
@@ -20,62 +15,15 @@ const form = useForm({
     ref="modal"
     class="modal-dialog"
   >
-    <form>
-      <div class="ion-padding">
-        <AppModalHeader
-          :icon="PhCurrencyCircleDollar"
-          title="Novo pagamento"
-          class="text-[var(--ion-color-success-shade)]"
-        />
+    <div class="ion-padding">
+      <AppModalHeader
+        :icon="PhCurrencyCircleDollar"
+        title="Novo pagamento"
+        class="text-[var(--ion-color-success-shade)]"
+      />
 
-        <IonGrid>
-          <IonRow>
-            <IonCol>
-              <AppInput
-                v-model="form.data.value"
-                v-maskito="currencyBrlMask"
-                class="mb-10"
-                name="value"
-                label="Valor"
-                placeholder="R$ "
-                inputmode="numeric"
-              >
-                <IonButton
-                  slot="end"
-                  shape="round"
-                  fill="clear"
-                >
-                  Resto
-                </IonButton>
-              </AppInput>
-            </IonCol>
-          </IonRow>
-
-          <IonRow>
-            <IonCol>
-              <IonButton
-                class="w-full min-h-12"
-                shape="round"
-                fill="clear"
-                color="dark"
-                @click="modal?.$el.dismiss()"
-              >
-                Cancelar
-              </IonButton>
-            </IonCol>
-            <IonCol>
-              <IonButton
-                class="w-full min-h-12"
-                shape="round"
-                color="success"
-              >
-                Pagar
-              </IonButton>
-            </IonCol>
-          </IonRow>
-        </IonGrid>
-      </div>
-    </form>
+      <EntryPaymentModalForm @cancel="modal?.$el.dismiss()" />
+    </div>
   </IonModal>
 </template>
 
---
+++ src/views/home/_partials/EntryPaymentModalForm.vue
@@ -0,0 +1,81 @@
+<script setup lang="ts">
+import { IonButton, IonCol, IonGrid, IonRow } from '@ionic/vue'
+import { maskito as vMaskito } from '@maskito/vue'
+import { DateTime } from 'luxon'
+
+import AppInput from '@/components/AppInput.vue'
+import { useForm } from '@/composables/use-form'
+import { currencyBrlMask } from '@/support/masks'
+
+const emit = defineEmits(['submitted', 'cancel'])
+
+const form = useForm({
+  value: 'R$ ',
+  date: DateTime.now().toISODate(),
+})
+
+const submit = () => {}
+</script>
+
+<template>
+  <form @submit.prevent="submit">
+    <IonGrid>
+      <IonRow>
+        <IonCol>
+          <AppInput
+            v-model="form.data.value"
+            v-maskito="currencyBrlMask"
+            class="mb-10"
+            name="value"
+            label="Valor"
+            placeholder="R$ "
+            inputmode="numeric"
+          >
+            <IonButton
+              slot="end"
+              shape="round"
+              fill="clear"
+            >
+              Resto
+            </IonButton>
+          </AppInput>
+        </IonCol>
+      </IonRow>
+
+      <IonRow class="ion-margin-bottom">
+        <IonCol>
+          <AppInput
+            v-model="form.data.date"
+            type="date"
+            label="Data do pagamento"
+            placeholder="dd/mm/aaaa"
+          />
+        </IonCol>
+      </IonRow>
+
+      <IonRow>
+        <IonCol>
+          <IonButton
+            class="w-full"
+            shape="round"
+            fill="clear"
+            color="dark"
+            @click="$emit('cancel')"
+          >
+            Cancelar
+          </IonButton>
+        </IonCol>
+        <IonCol>
+          <IonButton
+            class="w-full"
+            shape="round"
+            color="success"
+            type="submit"
+          >
+            Pagar
+          </IonButton>
+        </IonCol>
+      </IonRow>
+    </IonGrid>
+  </form>
+</template>
